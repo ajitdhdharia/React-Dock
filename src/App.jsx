f15eb86945bb567ed2853ff83bb3ba032d9b0458
ajitdhdharia <ajitdhdhariaaa@gmail.com>
2024-02-14 09:39:18 -0500

    Login Profile ContextAPI

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import CurrencyConverter from "./Projects/CurrencyConverter/CurrencyConverter";
 import "./App.css";
+import UserContextProvider from "./Projects/MiniContext/context/UserContextProvider";
+import Login from "./Projects/MiniContext/components/Login";
+import Profile from "./Projects/MiniContext/components/Profile";
+
 function App() {
   return (
     <>
       <CurrencyConverter />
+      <UserContextProvider>
+        <h1>Mini Context API</h1>
+        <Login />
+        <Profile />
+      </UserContextProvider>
     </>
   );
 }
